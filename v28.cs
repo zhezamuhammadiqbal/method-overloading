942c30004ba4e246d8a1f0ae122563bf87ef2933
zhezaaaaaaa <126034467+zhezamuhammadiqbal@users.noreply.github.com>
2023-03-15 12:38:44 +0700

    Create v28.cs

---
+++ v28.cs
@@ -0,0 +1,15 @@
+namespace v28
+{
+
+    class Rumus
+    {
+
+        public int persegi (int s){
+            return s * s;
+        }
+
+        public int persegi(int p, int l){
+            return p*l;
+        }
+    }
+}
